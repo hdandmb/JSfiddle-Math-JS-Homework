b948764f071ed3520e2b68181a339b5e330cf195
Mia Boyles <hdandmb@gmail.com>
2017-10-11 12:50:02 -0700

    added javascript code from Jsfiddle

---
+++ JS/math.js
@@ -0,0 +1,22 @@
+// Pick any number, save it as variable: x
+
+	var x = 12;
+
+//   Multiply x by 4, save as variable: y
+  var y = x * 4;
+
+  
+//   Multiply y by 5, save as variable: z
+  
+  var z = y * 5;
+
+//   Divide z from x, save as variable: a
+  var a = z/x;
+  
+//   Subtract 7 from a, save as variable: answer
+  
+	var answer = a - 7;
+
+//   console log answer to view your result
+  
+  console.log(answer); 
